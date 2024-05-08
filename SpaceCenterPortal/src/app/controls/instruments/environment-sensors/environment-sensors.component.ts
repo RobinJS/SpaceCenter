b0d4426cb7bb8d4731793788c8b972f05a798978
RobinJS <RobinJS@users.noreply.github.com>
2024-05-08 22:24:12 +0300

    added temperature warnings

--- SpaceCenterPortal/src/app/controls/instruments/environment-sensors/environment-sensors.component.ts
+++ SpaceCenterPortal/src/app/controls/instruments/environment-sensors/environment-sensors.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DeviceListService } from '../../../device-manager/device-list/device-list.service';
 import { EnvironmentSensors } from '../../../models/instruments/EnvironmentSensors';
 import { Rover } from '../../../models/devices/Rover';
+import { Message } from '../log/log.component';
+import { MessageService } from '../../../common/message/message.service';
 
 @Component({
   selector: 'app-environment-sensors',
@@ -11,10 +13,12 @@ import { Rover } from '../../../models/devices/Rover';
 export class EnvironmentSensorsComponent implements OnInit {
     environmentSensors: EnvironmentSensors;
     
-    constructor(private deviceListService: DeviceListService) {}
+    constructor(private deviceListService: DeviceListService, private messageService: MessageService) {}
 
     ngOnInit(): void {
         this.environmentSensors = (this.deviceListService.getSelectedDevice() as Rover).environmentSensors;
-        
+        this.environmentSensors.onLogMessage.subscribe((message: Message)=>{
+            this.messageService.newMessage(message);
+        });
     }
 }
--- SpaceCenterPortal/src/app/models/instruments/EnvironmentSensors.ts
+++ SpaceCenterPortal/src/app/models/instruments/EnvironmentSensors.ts
@@ -1,21 +1,29 @@
+import { EventEmitter } from "@angular/core";
 import { formatValue } from "../../utils/Utils";
 import { Battery } from "./Battery";
 import { Instrument } from "./Instrument";
+import { Message, MessageType } from "../../controls/instruments/log/log.component";
 
 export class EnvironmentSensors extends Instrument {
     private readonly minLight = 0.0;
     private readonly maxLight = 1.0;
-    private readonly minTemp = 84;
-    private readonly maxTemp = 86;
     private readonly minWind = 0;
     private readonly maxWind = 5;
+    private readonly minTemp = -125;
+    private readonly maxTemp = 85;
+    readonly minOperationalTemp = -125;
+    readonly maxOperationalTemp = 85;
 
     private _illumination:number;
     private _temperature:number;
     private _wind:number;
+    private lowTempWarningShown = false;
+    private highTempWarningShown = false;
     get illumination():number { return this._illumination; }
     get temperature():number { return this._temperature; }
     get wind():number { return this._wind; }
+
+    onLogMessage = new EventEmitter<Message>();
 
     constructor(battery: Battery) {
         super(battery);
@@ -34,8 +42,37 @@ export class EnvironmentSensors extends Instrument {
         // to do: get data from server
         this._illumination = formatValue(Math.random() * (this.maxLight - this.minLight) + this.minLight);
         this._temperature = formatValue(Math.random() * (this.maxTemp - this.minTemp) + this.minTemp);
+        
+        let extremeTempChance = Math.random() * 100;
+        if (extremeTempChance < 5) this._temperature = this.minTemp - 1;
+        else if (extremeTempChance < 10) this._temperature = this.maxTemp + 1;
+
         this._wind = formatValue(Math.random() * (this.maxWind - this.minWind) + this.minWind);
 
         this.battery.consume(this.consumption);
+
+        this.handleWarnings();
+    }
+
+    handleWarnings() {
+        if (this.temperature <= this.minOperationalTemp && !this.lowTempWarningShown) {
+            this.lowTempWarningShown = true;
+            this.onLogMessage.emit({text: "Warning! Low temperature!", type: MessageType.Warning});
+        }
+
+        if (this.temperature > this.minOperationalTemp && this.lowTempWarningShown) {
+            this.lowTempWarningShown = false;
+            this.onLogMessage.emit({text: "Temperature normalized.", type: MessageType.Default});
+        }
+
+        if (this.temperature >= this.maxOperationalTemp && !this.highTempWarningShown) {
+            this.highTempWarningShown = true;
+            this.onLogMessage.emit({text: "Warning! High temperature!", type: MessageType.Warning});
+        }
+
+        if (this.temperature < this.maxOperationalTemp && this.highTempWarningShown) {
+            this.highTempWarningShown = false;
+            this.onLogMessage.emit({text: "Temperature normalized.", type: MessageType.Default});
+        }
     }
 }
